384d814aaf2c6b7c2ba13a3567c6732cd72a74eb
runarer <95697611+runarer@users.noreply.github.com>
2025-12-18 14:43:37 +0100

    Basic Cereal class with parsing

---
+++ Cereal.cs
@@ -0,0 +1,50 @@
+class CerealAisle
+{
+    Cereal[] Cereals {get;}
+
+    public CerealAisle(string[] csvLines)
+    {
+        Cereals = [..csvLines.Select(l => new Cereal(l))];
+    }
+}
+
+class Cereal
+{
+    string name {get;}
+    string mfr {get;}// To be replaced by enum
+    string type {get;} // To be replaced by enum or bool
+    int calories {get;} 
+    int protein {get;} 
+    int fat {get;} 
+    int sodium {get;} 
+    double fiber {get;} 
+    double carbo {get;} 
+    int sugars {get;} 
+    int potass {get;} 
+    int vitamins {get;} 
+    int shelf {get;} 
+    int weight {get;} 
+    double cups {get;} 
+    decimal rating {get;} 
+
+    public Cereal(string csvLine)
+    {
+        string[] parsedValues = csvLine.Split(',',StringSplitOptions.RemoveEmptyEntries);
+        name = parsedValues[0];
+        mfr = parsedValues[1]; 
+        type = parsedValues[2];
+        calories = int.Parse(parsedValues[3]);
+        protein = int.Parse(parsedValues[4]); 
+        fat = int.Parse(parsedValues[5]); 
+        sodium = int.Parse(parsedValues[6]); 
+        fiber = Double.Parse(parsedValues[7]); 
+        carbo = Double.Parse(parsedValues[8]); 
+        sugars = int.Parse(parsedValues[9]); 
+        potass = int.Parse(parsedValues[10]); 
+        vitamins = int.Parse(parsedValues[11]); 
+        shelf = int.Parse(parsedValues[12]); 
+        weight = int.Parse(parsedValues[13]); 
+        cups = Double.Parse(parsedValues[14]); 
+        rating = Decimal.Parse(parsedValues[15]);
+    }
+}
