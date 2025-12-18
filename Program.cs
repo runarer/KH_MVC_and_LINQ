765f9f4e6a39a26042eae02d0fa1bcc7f80f4c3d
runarer <95697611+runarer@users.noreply.github.com>
2025-12-18 14:58:22 +0100

    Read file

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ string[] lines;
 try
 {
     lines = File.ReadAllLines("./cereal.csv");
+    Console.WriteLine(lines[0]);
 } catch (FileNotFoundException ex)
 {
     Console.WriteLine("File not found. This program requires 'cereal.csv' from 80-cereal from Kaggle.");
@@ -14,4 +15,3 @@ try
     Console.WriteLine(ex.Message);
     System.Environment.Exit(1);
 }
-
