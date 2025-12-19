41b129d1dbd0a974dd4f4b57d3d3acc6b3af349e
runarer <95697611+runarer@users.noreply.github.com>
2025-12-19 10:14:38 +0100

    Cereal  class, for storing data

--- Cereal.cs
+++ Cereal.cs
@@ -1,64 +1,36 @@
 
-
-class CerealAisle
+class Cereal(
+        string name,
+        string mfr,
+        bool cold,
+        int calories,
+        int protein,
+        int fat,
+        int sodium,
+        double fiber,
+        double carbo,
+        int sugars,
+        int potass,
+        int vitamins,
+        int shelf,
+        int weight,
+        double cups,
+        decimal rating)
 {
-    Cereal[] Cereals {get;}
-
-    public CerealAisle(string[] csvLines)
-    {
-        Cereals = [..csvLines.Select(l => new Cereal(l))];
-    }
-
-    public static string GetManufacturer(char c) => c switch
-    {
-        'A' => "American Home Food Products",
-        'G' => "General Mills",
-        'K' => "Kelloggs",
-        'N' => "Nabisco",
-        'P' => "Post",
-        'Q' => "Quaker Oats",
-        'R' => "Ralston Purina",
-        _ => "Unknown"
-    };
-}
-
-class Cereal
-{
-    string name {get;}
-    string mfr {get;}
-    bool cold {get;}
-    int calories {get;} 
-    int protein {get;} 
-    int fat {get;} 
-    int sodium {get;} 
-    double fiber {get;} 
-    double carbo {get;} 
-    int sugars {get;} 
-    int potass {get;} 
-    int vitamins {get;} 
-    int shelf {get;} 
-    int weight {get;} 
-    double cups {get;} 
-    decimal rating {get;} 
-
-    public Cereal(string csvLine)
-    {
-        string[] parsedValues = csvLine.Split(',',StringSplitOptions.RemoveEmptyEntries);
-        name = parsedValues[0];
-        mfr = CerealAisle.GetManufacturer(parsedValues[1][0]); 
-        cold = parsedValues[2] == "C";
-        calories = int.Parse(parsedValues[3]);
-        protein = int.Parse(parsedValues[4]); 
-        fat = int.Parse(parsedValues[5]); 
-        sodium = int.Parse(parsedValues[6]); 
-        fiber = Double.Parse(parsedValues[7]); 
-        carbo = Double.Parse(parsedValues[8]); 
-        sugars = int.Parse(parsedValues[9]); 
-        potass = int.Parse(parsedValues[10]); 
-        vitamins = int.Parse(parsedValues[11]); 
-        shelf = int.Parse(parsedValues[12]); 
-        weight = int.Parse(parsedValues[13]); 
-        cups = Double.Parse(parsedValues[14]); 
-        rating = Decimal.Parse(parsedValues[15]);
-    }
+    string Name {get;} = name;
+    string MFR {get;} = mfr;
+    bool Cold {get;} = cold;
+    int Calories {get;} = calories;
+    int Protein {get;} = protein; 
+    int Fat {get;} = fat;
+    int Sodium {get;} = sodium; 
+    double Fiber {get;} = fiber; 
+    double Carbohydrates {get;} = carbo;
+    int Sugars {get;} = sugars;
+    int Potass {get;} = potass; 
+    int Vitamins {get;} = vitamins;
+    int Shelf {get;} = shelf; 
+    int Weight {get;} = weight;
+    double Cups {get;} = cups;
+    decimal Rating {get;} = rating;
 }
