cd33d219bf3f278e6a17f96a071f1e5b2049e0e1
runarer <95697611+runarer@users.noreply.github.com>
2025-12-19 12:19:26 +0100

    Fixes null refrence warnings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿string filename = "./cereal.csv";
+﻿
+
+string filename = "./cereal.csv";
 
 Cereals? cereals=null;
 try {
@@ -16,31 +18,37 @@ while(userInput != "q" && userInput != "Q")
     userInput = Console.ReadLine();
     if(string.IsNullOrEmpty(userInput))
         continue;
-    if(userInput.Length == 1)
-        switch(userInput[0])
+    if(userInput.Length == 1) {
+        switch(userInput[0]) 
         {
             // List all cereals
             case '1':
-                
+                CerealView.ListCereals(cereals?.GetListOfCereals() ?? []);
                 break;
 
             // List all cereals best served cold
             case '2':
-
+                CerealView.ListCereals(cereals?.GetListOfCereals(true) ?? []);
                 break;
 
             // List all cereals best served hot
             case '3':
-
+                CerealView.ListCereals(cereals?.GetListOfCereals(false) ?? []);
                 break;
             
             // List cereals by manufactures
             case '4': 
             
                 break;
-            
+            case 'q':
+                return;
+            case 'Q':
+                return;            
             default: 
                 break;
         }
+        CerealView.DisplayContinueMessage();
+        string? anyKey = Console.ReadLine();
+    }
 }
 
